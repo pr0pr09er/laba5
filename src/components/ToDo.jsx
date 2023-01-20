99f336afa8a6003bead61201eae9a36f36dcf12d
pr0pr09er <113778429+pr0pr09er@users.noreply.github.com>
2023-01-20 13:21:45 +0300

    Add files via upload

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -3,22 +3,24 @@ import { useState } from "react";
 import "../App.css";
 function ToDo() {
   const [tasks, setTasks] = useState([
-    {id: 1, name: "Task1", state: true},
-    {id: 2, name: "Task2", state: true},
-    {id: 3, name: "Task3", state: true},
+    { id: 1, name: "Task1", state: true },
+    { id: 2, name: "Task2", state: true },
+    { id: 3, name: "Task3", state: true },
   ]);
 
   const [id, setId] = useState(4);
-  const [title, setTitle] = useState('');
-  const [editName, setEditName] = useState('');
-  const [checked, setChecked] = useState(true);
+  const [title, setTitle] = useState("");
+  const [editName, setEditName] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [editedId, setEditedId] = useState(null);
+
   const addNew = () => {
     let obj = {
       id: id,
       name: title,
     };
     const copy = Object.assign([], tasks);
-    setTitle('');
+    setTitle("");
     copy.push(obj);
     setTasks(copy);
     setId(id + 1);
@@ -28,47 +30,71 @@ function ToDo() {
     setTasks(tasks.filter((item) => item.id !== id));
   };
 
-  const editElement = (item) => {
-    const data = {
-      id: item.id,
-      name: editName,
+  const changeText = (value) => {
+    if (editedId != null) {
+      setEditName(value);
+      const data = {
+        id: editedId,
+        name: value,
+        state: true
+      };
+      setTasks(tasks.map((task) => (task.id === editedId ? data : task)));
     }
-    setTasks(tasks.map(task => task.id === item.id ? data : task))
-    setEditName('')
   };
 
-  function changeCheckbox (item) {
-    setChecked(item.state);
-    item.state = !checked;
+  const startEdit = (item) => {
+    setEditedId(item.id);
+    setEditName(item.name);
+  };
 
+  const stopEdit = () => {
+    console.log("ok");
+    setEditedId(null);
+    setEditName("");
+  };
+
+  function changeCheckbox(item) {
+    setChecked(item.state);
+    item.state = !item.state;
   }
 
   return (
-    <div className="wrapper">
+    <div>
       <div className="header">
         <div className="todo__header">
           <h1>todos</h1>
         </div>
       </div>
-      <div className="">
-        <div>
-          <input onChange={event => setTitle(event.target.value)} type="text" />
-          <button onClick={addNew}>Submit</button>
+      <div className="main">
+        <div className="root">
+          <input
+            className="mainInput"
+            onChange={(event) => setTitle(event.target.value)}
+            type="text"
+          />
+          <button className="btn-none" onClick={addNew}>
+            Submit
+          </button>
         </div>
-        <div>
+        <div className="main__tasks">
           {tasks.map((item) => (
-            <div key={item.id}>
-              <input type="checkbox" checked={item.state} onChange={() => changeCheckbox(item)} id={item.id} />
-              <p onClick={() => editElement(item)}>
+            <div key={item.id} className="main__task">
+              <input
+                type="checkbox"
+                onChange={() => changeCheckbox(item)}
+                id={item.id}
+              />
+              <p className={item.state ? "notChecked" : "checked"} onClick={() => startEdit(item)}>
                 {item.id}/{item.name}
               </p>
-              <button onClick={() => removeElement(item.id)}>Delete</button>
+              <button className="main__btn" onClick={() => removeElement(item.id)}>Delete</button>
             </div>
           ))}
-          <input
+          <input className="editInput"
             value={editName}
+            onBlur={stopEdit}
             type="text"
-            onChange={(event) => setEditName(event.target.value)}
+            onChange={(event) => changeText(event.target.value)}
           />
         </div>
         <div></div>
